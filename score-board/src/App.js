4f43a223c93339b44ce712c4844935f1ec236e52
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-01-17 11:08:45 -0500

    MVP met

--- score-board/src/App.js
+++ score-board/src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Display from './components/Display';
+import Dashboard from './components/Dashboard';
 
 function App() {
+  const [strikes, setStrikes] = useState(0);
+  const [balls, setBalls] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Display strikes = {strikes} balls={balls} />
+      <Dashboard strikes={strikes} balls={balls} setStrikes={setStrikes} setBalls={setBalls} />
     </div>
   );
 }
